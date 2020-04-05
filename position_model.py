c3560cebd1f16206b2cd6cf4fca5a315a535c3db
Nick DeFrancis <ndefrancis@fb.com>
2020-04-04 18:18:40 -0700

    agg function with weights

--- position_model.py
+++ position_model.py
@@ -1,6 +1,7 @@
 from data_cleaner import GetData, GetLECLCS, GetRecent
 import numpy as np
 from agg_functions import GetRawAvgsDF
+import pandas as pd
 
 df = GetRecent()
 
@@ -8,7 +9,6 @@ curseason = '2020-1'
 
 df['iscurrent'] = df['split'].apply(lambda x: 1 if x.startswith(curseason) else 0)
 
-print(df.head(), df.tail())
 
 raw_avg = GetRawAvgsDF(df)
 
@@ -16,11 +16,20 @@ df['weight'] = df['iscurrent'].apply(lambda x: 1.0 if x == 1 else 0.3)
 
 
 # calculated weighted means -- confirmed via excel this function works
-test = df.groupby(['player', 'result']).apply(lambda dfx: (dfx["fpts"] * dfx["weight"]).sum() / dfx["weight"].sum())
+test = df.groupby(['player', 'position', 'result']).apply(lambda dfx: (dfx["fpts"] * dfx["weight"]).sum() / dfx["weight"].sum())
+test.rename('avg_fpts')
+test.to_frame()
+
+g = df.groupby(['player','position', 'result'])
+newdf = g.apply(lambda x: pd.Series([np.average(x['fpts'], weights=x['weight']),
+                             np.count_nonzero(x['weight'])],
+                                    index=['fpts','count'])).unstack()
+
+
+print(newdf.head(10))
+
 
 ## STILL NEED TO ADD 5 ROWS FROM
-
-print(test.head())
 
 
 
